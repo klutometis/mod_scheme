c5f5565320a02640b48ca1a3740c148f06cd04ed
Peter Danenberg <pcd@roxygen.org>
2011-03-11 05:27:13 -0800

    rudimentary error-catching

--- prelude.scm
+++ prelude.scm
@@ -1,6 +1,7 @@
 (include "srclib/environments/environments.scm")
 (import extras ports srfi-13 environments defstruct foreigners)
 (foreign-declare "#include <httpd.h>")
+(foreign-declare "#include <http_log.h>")
 ;;; test this against just sending it all along.
 
 (defstruct
@@ -31,28 +32,84 @@
   #;((c-pointer "apr_pool_t") request-pool)
   (int header_only request-rec-header-only))
 
-(define (make-request-output-port request*)
+(define (puts string request-rec*)
+  ((foreign-lambda int
+                   "ap_rputs"
+                   c-string
+                   (c-pointer "request_rec"))
+   string
+   request-rec*))
+
+(define (log-error request-rec* string)
+  ((foreign-lambda void
+                   "ap_log_rerror"
+                   c-string
+                   int
+                   int
+                   int
+                   (c-pointer "request_rec")
+                   c-string)
+   ;; something more instructive, perhaps?
+   (foreign-value "__FILE__" c-string)
+   (foreign-value "__LINE__" int)
+   (foreign-value "LOG_DEBUG" int)
+   (foreign-value "OK" int)
+   request-rec*
+   string))
+
+(define (make-request-output-port request-rec*)
   (let ((buffer ""))
     (make-output-port
      (lambda (scribendum)
        (set! buffer (string-append/shared buffer scribendum)))
      (lambda () (set! buffer ""))
-     (lambda () ((foreign-lambda int
-                                 "ap_rputs"
-                                 c-string
-                                 (c-pointer "request_rec"))
-                 buffer
-                 request*)))))
+     (lambda () (rputs buffer request-rec*)))))
 
 (define-external
-  (mod_scheme_handle (c-string file) (c-pointer request*))
+  (mod_scheme_handle (c-string file) (c-pointer request-rec*))
   int
   (let ((environment
          (environment-copy #;(scheme-report-environment 5)
           (interaction-environment)
           #t)))
-    (load file (lambda (expression) (eval expression environment)))
-    (eval `(handle! (make-request ,(request-rec-header-only request*)))
-          environment)))
+    (condition-case
+     (begin
+       (load file (lambda (expression)
+                    (eval expression environment)))
+       (condition-case
+        (eval `(handle! (make-request ,(request-rec-header-only request-rec*)))
+              environment)
+        (exn (exn)
+             (let ((message
+                    ((condition-property-accessor 'exn 'message)
+                     exn))
+                   (arguments
+                    ((condition-property-accessor 'exn 'arguments)
+                     exn))
+                   (location
+                    ((condition-property-accessor 'exn 'location)
+                     exn)))
+               (log-error request-rec*
+                          (format "Message: ~A; Arguments: ~A; Location: ~A"
+                                  message
+                                  arguments
+                                  location))
+               (foreign-value "HTTP_INTERNAL_SERVER_ERROR" int)))))
+     (exn (exn)
+          (let ((message
+                 ((condition-property-accessor 'exn 'message)
+                  exn))
+                (arguments
+                 ((condition-property-accessor 'exn 'arguments)
+                  exn))
+                (location
+                 ((condition-property-accessor 'exn 'location)
+                  exn)))
+            (log-error request-rec*
+                       (format "Message: ~A; Arguments: ~A; Location: ~A"
+                               message
+                               arguments
+                               location))
+            (foreign-value "HTTP_INTERNAL_SERVER_ERROR" int))))))
 
 (return-to-host)
--- test-handle.scm
+++ test-handle.scm
@@ -1,2 +1,7 @@
+;; (import scheme chicken)
+(require-library environments)
+(import environments)
+
 (define (handle! request)
+  (make-environment ounth)
   (request-header-only request))
